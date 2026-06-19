4e4cb7450e79b0695e9d908391bc1d4e00cf0871
yasnoponyal17 <snootfornoyeah@yandex.ru>
2026-06-19 16:23:23 +0300

    w

--- server.js
+++ server.js
@@ -8,29 +8,28 @@ const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 
-const uploadFile = upload.single('file')
+const uploadFile = upload.any()
 
 app.get('/login', function (req, res) {
     res.send('1155290')
 })
 
 app.post('/zipper', uploadFile, async function (req, res, next) {
-    console.log('req.file:', req.file)
-    console.log('req.body:', req.body)
+    console.log('req.files:', req.files)
 
-    if (!req.file) {
+    if (!req.files || req.files.length === 0) {
         return res.status(400).send('Файл не получен')
     }
 
+    const file = req.files[0]
+
     try {
-        const inputBuffer = fs.readFileSync(req.file.path)
-        const compressedBuffer = await gzipAsync(inputBuffer, { level: 9 });
-
-
+        const inputBuffer = fs.readFileSync(file.path)
+        const compressedBuffer = await gzipAsync(inputBuffer, { level: 9 })
         res.setHeader('Content-Type', 'application/gzip')
         res.send(compressedBuffer)
     } catch (error) {
-        console.error('Ошибка:', error);
+        console.error('Ошибка:', error)
         res.status(500).send('Ошибка сжатия')
     }
 })
