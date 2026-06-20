0bee8099fcd3a11b6530e723bc0ab64a597abb25
yasnoponyal17 <snootfornoyeah@yandex.ru>
2026-06-20 19:04:46 +0300

    j

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 const express = require('express')
-const multer = require('multer')
+const Busboy = require('busboy')
 const zlib = require('zlib')
 const fs = require('fs')
-const { promisify } = require('util');
-const gzipAsync = promisify(zlib.gzip);
-const upload = multer({ dest: 'uploads/' })
+const { promisify } = require('util')
+const gzipAsync = promisify(zlib.gzip)
 
 const app = express()
-
-const uploadFile = upload.single('file')
 
 app.get('/login', function (req, res) {
     res.send('1155290')
 })
 
-app.post('/zipper', uploadFile, async function (req, res, next) {
-    try {
-        const inputBuffer = fs.readFileSync(req.file.path)
-        const compressedBuffer = await gzipAsync(inputBuffer, { level: 9 });
+app.post('/zipper', function (req, res) {
+    const busboy = Busboy({ headers: req.headers })
+    const chunks = []
 
+    busboy.on('file', (fieldname, file, info) => {
+        file.on('data', (data) => {
+            chunks.push(data)
+        })
+    })
 
-        res.setHeader('Content-Type', 'application/gzip')
-        res.send(compressedBuffer)
-    } catch (error) {
-        console.error('Ошибка:', error);
-    }
+    busboy.on('finish', async () => {
+        const inputBuffer = Buffer.concat(chunks)
+        try {
+            const compressedBuffer = await gzipAsync(inputBuffer, { level: 9 })
+            res.setHeader('Content-Type', 'application/gzip')
+            res.send(compressedBuffer)
+        } catch (error) {
+            res.status(500).send('Ошибка сжатия')
+        }
+    })
+
+    req.pipe(busboy)
 })
 
 app.listen(process.env.PORT || 3000, () => {
